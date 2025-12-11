c4f9bd3b56661ecff02e9c5278baf6f82a22523a
fermeridamagni <hello@magni.dev>
2025-12-11 12:23:35 -0600

    feat: enhance Metro configuration to support monorepo structure and symlink resolution

--- apps/mobile-app/metro.config.js
+++ apps/mobile-app/metro.config.js
@@ -1,8 +1,21 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativewind } = require("nativewind/metro");
+const path = require("node:path");
 
 /** @type {import('expo/metro-config').MetroConfig} */
-const config = getDefaultConfig(__dirname);
+const projectRoot = path.dirname(__filename);
+const workspaceRoot = path.resolve(projectRoot, "../..");
+
+const config = getDefaultConfig(projectRoot);
+
+// 1. Watch all files within the monorepo
+config.watchFolders = [workspaceRoot];
+// 2. Let Metro know where to resolve packages and in what order
+config.resolver.nodeModulesPaths = [
+  path.resolve(projectRoot, "node_modules"),
+  path.resolve(workspaceRoot, "node_modules"),
+];
+config.resolver.unstable_enableSymlinks = true;
 
 module.exports = withNativewind(config);
